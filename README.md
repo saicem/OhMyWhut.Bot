d6d8af3fdbb1904e10f225376d5ea1d50f8153f9
saicem <saicem@qq.com>
2022-10-30 16:12:26 +0800

    『docs』添加说明及注释

---
+++ README.md
@@ -0,0 +1,14 @@
+# OhMyWhut.Bot
+
+一个机器人，可用于武理的电费、课表、图书查询。
+
+## 依赖
+
+- [oicq](https://github.com/takayama-lily/oicq)
+- [OhMyWhut.FastFetcher](https://github.com/saicem/OhMyWhut.FastFetcher)
+
+## 开发
+
+修改 [config.ts](src/config.ts)
+
+在 [controller](src/controller) 文件夹内编写控制器，注入 [main.ts](src/main.ts) 即可
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,26 @@
 export default {
+  /**
+   * 机器人账号
+   */
   username: 0,
+
+  /**
+   * 机器人密码
+   */
   password: "",
+
+  /**
+   * 管理员账号,用于通知
+   */
   admin: 0,
+
+  /**
+   * 过滤长度，超过此长度的消息会被忽略
+   */
   filterLength: 80,
+
+  /**
+   * 依赖的 api 链接，查看 https://github.com/saicem/OhMyWhut.FastFetcher
+   */
   fastFetcherUrl: "",
 };
